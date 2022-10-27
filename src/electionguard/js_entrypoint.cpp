29e9a3658a536011d37822cd1887b60c6795f001
Lee Richardson <github@leerichardson.com>
2022-10-27 10:37:30 -0400

    Print encryption duration

--- src/electionguard/js_entrypoint.cpp
+++ src/electionguard/js_entrypoint.cpp
@@ -8,6 +8,10 @@
 #include "electionguard/ballot.hpp"
 #include "electionguard/ballot.h"
 
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
 using std::string;
 using electionguard::CiphertextElectionContext;
 using electionguard::Manifest;
@@ -18,6 +22,10 @@ using electionguard::PlaintextBallot;
 
 const char *js_encrypt(const char *context, const char *manifest, const char *ballot)
 {
+    // duration calculation
+    auto start = std::chrono::system_clock::now();
+    std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+    std::cout << "started encryption at " << std::ctime(&start_time) << std::endl;
     // get context
     auto context_obj = CiphertextElectionContext::fromJson(string(context));
     // get manifest 
@@ -31,8 +39,16 @@ const char *js_encrypt(const char *context, const char *manifest, const char *ba
     auto ballot_obj = PlaintextBallot::fromJson(string(ballot));
     // encrypt
     auto ciphertext = mediator->encrypt(*ballot_obj);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "finished encryption at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s"
+              << std::endl;
     // convert to json
+    std::cout << "encryption successful" << std::endl;
     auto json = ciphertext->toJson(false);
+    std::cout << "toJson successful" << std::endl;
 
     delete AS_TYPE(eg_ciphertext_election_context_t, context_obj.release());
     // or maybe: delete AS_TYPE(eg_context_configuration_t, context_obj.release());
